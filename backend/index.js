b80425e30c2148ba166587b76298593cfed9d0ab
Mohit Uniyal <mohituniyal46@gmail.com>
2024-08-09 18:58:41 +0530

    fixed missed cors in backend

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ const server = app.listen(PORT, () => {
 const io = new Server(server, {
   // pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
     credentials: true,
   },
 });
