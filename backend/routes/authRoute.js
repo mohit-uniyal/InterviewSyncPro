50c429de89473209d6ac0341afa4eee2ab901057
Mohit Uniyal <mohituniyal46@gmail.com>
2024-08-09 14:59:51 +0530

    backend commited for deploying-1

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router();
+const {registerController,loginController, getuserController} = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+// Get loggedin User details using POST "/api/auth/getuser". Login required
+router.post("/getuser", authMiddleware, getuserController);
+
+
+module.exports = router;
